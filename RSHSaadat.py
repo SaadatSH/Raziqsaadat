8d008397c3200e77fa67bef7371955f24df0ce6d
SaadatSH <130240466+SaadatSH@users.noreply.github.com>
2023-04-08 21:19:56 +0430

    RSHSaadat.py
    rm -rf RSHSaadat
    git clone 
    https://github.com/SaadatSH/Raziqsaadat.git
    ls
    cd RSHSaadat
    ls
    python RSHSaadat.py

---
+++ RSHSaadat.py
@@ -0,0 +1,90 @@
+^?ELF^B^A^A^@^@^@^@^@^@^@^@^@^C^@�^@^A^@^@^>
+^@@^@ ^@^^^@^F^@^@^@^D^@^@^@@^@^@^@^@^@^@^@>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^Q^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^P^@^@^@^@^@^@^@c^M^E^@^@^@^B^@y
+^@^@^@^@^@^@^@^@^@^@^A^@^@^@�^@^@^@^A^@^@^@>
+�^@^@^@�qX^\���|BE���nE��^Z�Z�^@^@^@�^@^@^@>
+^@^@^@T^@^@^@~^@^@^@�^@^@^@�^@^@^@1^@^@^@�^>
+^?^@^@^@^@^@^@�,^C^@^@^@^@^@^C^D^@^@^@^@^@^>
+^@^@^@^@^@^@^@^@^@^@^@�M^C^@^@^@^@^@^B^D^@^>
+  ^[[1;36m $$$$$$\   $$$$$$\   $$$$$$\ $$$$$$$$\  $$$$$$\  
+$$  __$$\ $$  __$$\ $$  __$$\\__$$  __|$$  __$$\ 
+$$ /  \__|$$ /  $$ |$$ /  $$ |  $$ |   $$ /  $$ |
+\$$$$$$\  $$$$$$$$ |$$$$$$$$ |  $$ |   $$$$$$$$ |
+ \____$$\ $$  __$$ |$$  __$$ |  $$ |   $$  __$$ |
+$$\   $$ |$$ |  $$ |$$ |  $$ |  $$ |   $$ |  $$ |
+\$$$$$$  |$$ |  $$ |$$ |  $$ |  $$ |   $$ |  $$ |
+ \______/ \__|  \__|\__|  \__|  \__ ^[[1:39m
+
+(!)═══════════════════════════════════════
+
+(!) Author   : Saadat
+
+(!) Guthub   : Saadat110
+
+(!) Facebook : Raziqullah saaadat.07
+
+(!) Type     : Trail
+
+(!) Enjoy Trail Share SS in Group ❤
+^[[1;37m(!)════════════════════════════════>
+
+^[[1;37m(!)════════════════════════════════>
+
+^[[1;37m[2] Random Cloning ^[[1;32m[M2]
+
+^[[1;37m[0] Exit Menu ^@2^@^[[38;5;208m^@6^>
+
+[•] Total OK Ids 1000 2000 3000 50000 100000 : ^@ConnectionError^@D^@Di>
+
+[•] Limint : ^@F^@G^@H^@I^@
+
+(!) Info : Something Working Was Wrong^@J^@>
+
+[•] CP IDS SAVE : /sdcard/AKING-CP.txt^@P^@>
+
+[•] BD Code Afg: 077,079,078.070,,etc...
+
+[•] Code : ^@[•] Press Enter To Back Menu ^>
+
+[•] Total CP IDs : ^@U^@V^@ValueError^@W^@X>
+•] BD Code: 880180,880191,880174,etc...
+
+[•] Code : ^@[•] Press Enter To Back Menu ^>
+
+[•] Total CP IDs : ^@U^@V^@ValueError^@W^@X>
+
+^@^M^@; ^@.^@(^@!^@) ^@  ^@^M ^@[^@a^@aa^@a>
+
+return next yielded value or raise StopIter>
+
+return next yielded value or raise StopIter>
+
+    try: Generator = _module.Generator
+
+    except AttributeError: pass
+
+    else: Generator.register(_cython_genera>
+
+if _cython_coroutine_type is not None:
+
+    try: Coroutine = _module.Coroutine
+
+    except AttributeError: pass
+
+^@need more than %zd value%.1s to unpack^@_>
+^@^@��^@^@
+^@^@x�^@^@@
+^@^@^P�^@^@`
+^@^@��^@^@�
+^@^@8�^@^@�
+^@^@x�^@^@�
+^@^@^T�^@^@�
+^@^@��^@^@^@^K^@^@��^@^@ ^K^@^@��^@^@@^K^@^>
+�^L�^N�^P�^R�^T�^V�^X^@^@^@^\^@^@^@^\^A^@^@>
+�^L^@^\^@^@^@\^A^@^@��^@^@�^@^@^@^@L^L^] �^>
+    else: Coroutine.register(_cython_corout>
